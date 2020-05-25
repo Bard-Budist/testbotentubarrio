63c59f088d2a77db022ec2408bc47040b6a09e51
Bard-Budist <op7puro@live.com>
2020-05-25 00:32:55 -0500

    Restore commit

--- index.js
+++ index.js
@@ -2,47 +2,16 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-var mysql = require('mysql');
 const restService = express();
 
 restService.use(bodyParser.json());
 
 restService.post("/", function(req, res) {
-  //res.send(JSON.stringify(req.body.queryResult.queryText));
+  res.send(JSON.stringify(req.body.queryResult.queryText));
 
-  var con = mysql.createConnection({
-    host: "181.63.179.1",
-    user: "pepito",
-    password: "",
-    database: "messages"
-  });
-
-  con.connect(function(err) {
-    if (err) throw err;
-    res.send(JSON.stringify({'Status':'OK DB'}));
-    var sql = "INSERT INTO base (msg) VALUES ('" + req.body.queryResult.queryText +"')";
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      res.send(JSON.stringify({'Insert':'OK'}));
-      
-    });
-  });
+  
 });
-  restService.listen(process.env.PORT || 8000, function() {
-    console.log("Server up and listening");
-  });
 
-
-
-
-/*
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("1 record inserted");
-  });
+restService.listen(process.env.PORT || 8000, function() {
+  console.log("Server up and listening");
 });
-*/
