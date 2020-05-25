a9ac37d3fb15f2c7d8107353ff5853507420e8f4
Bard Budist <44384347+Bard-Budist@users.noreply.github.com>
2020-05-24 22:18:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,37 +13,8 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-restService.post("/Ciudad", function(req, res) {
-  var speech =
-    req.body.queryResult &&
-    req.body.queryResult.parameters &&
-    req.body.queryResult.parameters.location
-      ? req.body.queryResult.parameters.location
-      : "Seems like some problem. Speak again.";
-  
-  var speechResponse = {
-    google: {
-      expectUserResponse: true,
-      richResponse: {
-        items: [
-          {
-            simpleResponse: {
-              textToSpeech: speech
-            }
-          }
-        ]
-      }
-    }
-  };
-  
-  return res.json({
-    payload: speechResponse,
-    //data: speechResponse,
-    fulfillmentText: speech,
-    speech: speech,
-    displayText: speech,
-    source: "testbotentubarrio"
-  });
+restService.post("/", function(req, res) {
+  res.send("Good work");
 });
 
 restService.listen(process.env.PORT || 8000, function() {
