53261269b085469ccb3fd799f3971ee745426a15
Bard-Budist <op7puro@live.com>
2020-05-25 12:42:49 -0500

    Add token

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ restService.use(bodyParser.json());
 
 restService.post("/", function(req, res) {
   //res.send(JSON.stringify(req.body.queryResult.queryText));
-
+  //Connection DB
   var con = mysql.createConnection({
     host: "181.63.179.1",
     user: "pepito",
@@ -17,23 +17,52 @@ restService.post("/", function(req, res) {
     database: "messages"
   });
 
+
   con.connect(function(err) {
     if (err) {
       console.log("Error");
     }
     //res.send(JSON.stringify({'Status':'OK DB'}));
+    //String query
     var sql = "INSERT INTO base (msg) VALUES ('" + req.body.queryResult.queryText +"')";
     con.query(sql, function (err, result) {
       if (err) {
         console.log("Error");
-        
       }
-      
-      
     });
     res.send(JSON.stringify({'Insert':'OK'}));
   });
   
+});
+
+
+
+
+restService.get('/', (req, res) => {
+
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "EAALirSQUH18BAPHJAr6aaZAxIGXy1LMjxsMNc8DQtJHh6MDagCeHPVp5eVkD2xCZAm3IDI8yZCH43cTLEIxzP5jKbJ6LpBuPFfRJ31r72pelJUzeAZBZBXPJlOIeznmpbqovMtE9fJk9beWTf3kdQEYeB94lolfZC2AcZAz3yXpeGSv5gKbON2F"
+    
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+    
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+  
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+    }
+  }
 });
 
 restService.listen(process.env.PORT || 8000, function() {
