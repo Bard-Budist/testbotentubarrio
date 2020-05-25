cf668f20ebdc43d46c812329151a983c75a9f8bd
Bard Budist <44384347+Bard-Budist@users.noreply.github.com>
2020-05-24 21:33:45 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 'use strict';
 
-// Imports dependencies and set up http server
-const
-  express = require('express'),
-  bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()); // creates express http server
+const express = require("express");
+const bodyParser = require("body-parser");
 
-// Sets server port and logs message on success
-app.listen(process.env.PORT || 8000, () => console.log('webhook is listening'));
+const restService = express();
 
+restService.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-// Creates the endpoint for our webhook 
-app.post('/ciudad', (req, res) => {  
- 
-  let body = req.body;
+restService.use(bodyParser.json());
 
+restService.post("/Ciudad", function(req, res) {
   var speech =
     req.body.queryResult &&
     req.body.queryResult.parameters &&
@@ -22,55 +21,32 @@ app.post('/ciudad', (req, res) => {
       ? req.body.queryResult.parameters.location
       : "Seems like some problem. Speak again.";
   
-  console.log(speech);
-  
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
-
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-    });
-
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-
-});
-
-// Adds support for GET requests to our webhook
-/*
-app.get('/webhook', (req, res) => {
-
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
-    
-  // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-    
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-  
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-    
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
+  var speechResponse = {
+    google: {
+      expectUserResponse: true,
+      richResponse: {
+        items: [
+          {
+            simpleResponse: {
+              textToSpeech: speech
+            }
+          }
+        ]
+      }
     }
-  }
+  };
+  
+  return res.json({
+    payload: speechResponse,
+    //data: speechResponse,
+    fulfillmentText: speech,
+    speech: speech,
+    displayText: speech,
+    source: "testbotentubarrio"
+  });
 });
-*/
+
+restService.listen(process.env.PORT || 8000, function() {
+  console.log("Server up and listening");
+});
+
