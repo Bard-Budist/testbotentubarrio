52ce7903626bad431bb6c5e42e3479de8cd9422d
Bard-Budist <op7puro@live.com>
2020-05-25 08:05:58 -0500

    Commit for test db

--- index.js
+++ index.js
@@ -3,12 +3,36 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const restService = express();
+var mysql = require('mysql');
 
 restService.use(bodyParser.json());
 
 restService.post("/", function(req, res) {
-  res.send(JSON.stringify(req.body.queryResult.queryText));
+  //res.send(JSON.stringify(req.body.queryResult.queryText));
 
+  var con = mysql.createConnection({
+    host: "181.63.179.1",
+    user: "pepito",
+    password: "",
+    database: "messages"
+  });
+
+  con.connect(function(err) {
+    if (err) {
+      console.log("Error");
+    }
+    //res.send(JSON.stringify({'Status':'OK DB'}));
+    var sql = "INSERT INTO base (msg) VALUES ('" + req.body.queryResult.queryText +"')";
+    con.query(sql, function (err, result) {
+      if (err) {
+        console.log("Error");
+        
+      }
+      
+      
+    });
+    res.send(JSON.stringify({'Insert':'OK'}));
+  });
   
 });
 
