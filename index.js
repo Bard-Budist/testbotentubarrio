eadf94e440734eeb6292fcb2f1b080be7794b7c4
danielcinome <danielchinome987@gmail.com>
2020-05-26 22:10:57 +0000

    Add button in Card}

--- index.js
+++ index.js
@@ -25,14 +25,40 @@ function newSesion(agent) {
   return requesthttp.get("https://graph.facebook.com/" + id + "?fields=first_name,last_name&access_token=" + URLTOKEN).then(jsonBody => {
     const body = JSON.parse(jsonBody);
     // Add response with a card and name of user}
-    const card = new Card({
+    let response = {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [
+           {
+             title: body.first_name + ` Bienvenido`,
+             subtitle: 'Soy Elin el bot de EnTuBarrio',
+             image_url: 'https://lh3.googleusercontent.com/proxy/GcA6CqAzJ94Q8GMS9RgKYkys-xXNX93K_JC0b8VuXj7oMcDcztpAX1hOZlZNfyEDQYyi12jwPBRqx1jkSuPtrl9XulREZF13ItQa2tkSWbxwfQBmQjVRqdkVNBz59ydfGWlCI8c_r4yCsgkzr4FyOagndcB1CQAhHglk6Y7nWgm_mtZjexI',
+              buttons: [
+                {
+                  title: 'Comenzar Orden',
+                  type: 'postback',
+                  payload: 'comenzar'
+                },
+                {
+                  title: 'Soporte',
+                  type: 'postback',
+                  payload: 'soporte'
+                }
+              ]
+            }
+          ]
+        }
+      }
+    };
+    agent.add(response);
+    /*const card = new Card({
       imageUrl: 'https://lh3.googleusercontent.com/proxy/GcA6CqAzJ94Q8GMS9RgKYkys-xXNX93K_JC0b8VuXj7oMcDcztpAX1hOZlZNfyEDQYyi12jwPBRqx1jkSuPtrl9XulREZF13ItQa2tkSWbxwfQBmQjVRqdkVNBz59ydfGWlCI8c_r4yCsgkzr4FyOagndcB1CQAhHglk6Y7nWgm_mtZjexI',
       title: body.first_name + ` Bienvenido`,
       text: 'Soy Elin el bot de EnTuBarrio',
-    });
-    card.setButton1({text: 'Comenzar orden', url: 'comenzar orden'});
-    card.setButton2({text: 'Soporte', url: 'soporte'});
-    agent.add(card);  
+    });*/
+ 
     return Promise.resolve( agent );
 });
 }
