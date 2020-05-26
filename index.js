91cb973792134cdf5120f63c872f383cc4034cf3
danielcinome <danielchinome987@gmail.com>
2020-05-26 03:31:40 +0000

    Change code, use base of dialogflow

--- index.js
+++ index.js
@@ -1,61 +1,50 @@
 'use strict';
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const restService = express();
-var mysql = require('mysql');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
 const request = require('request');
-///const {WebhookCliente} = require('dialogflow-fulfillment');
+ 
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
-restService.use(bodyParser.json());
-restService.use(bodyParser.urlencoded({ extended: false }));
-
-//Endpoint home
-restService.post("/", function(req, res) {
-  //Connection DB
-
-  //const agent = new WebhookCliente({request, response});
-  //console.log('Request header'+ JSON.stringify(request.header));
-  //console.log('Request header', JSON.stringify(request.header));
-  
-  let id = req.body.originalDetectIntentRequest.payload.data.sender.id;
-  console.log(req.body);
-  //function probar(agent){
-    request("https://graph.facebook.com/" + id + "?fields=first_name,last_name&access_token=EAALirSQUH18BAPHJAr6aaZAxIGXy1LMjxsMNc8DQtJHh6MDagCeHPVp5eVkD2xCZAm3IDI8yZCH43cTLEIxzP5jKbJ6LpBuPFfRJ31r72pelJUzeAZBZBXPJlOIeznmpbqovMtE9fJk9beWTf3kdQEYeB94lolfZC2AcZAz3yXpeGSv5gKbON2F", function (error, response, body) {
-      console.error('error:', error); // Print the error if one occurred
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-      //console.add('Hola', body.first_name); // Print the HTML for the Google homepage.
-      res.json({
-        "fulfillmentMessages": body.first_name
-      });
-      
-    });
-    //agent.add('Hola', body.first_name);
-  //}
-
-  var con = mysql.createConnection({
-    host: "181.63.179.1",
-    user: "pepito",
-    password: "",
-    database: "messages"
+// This parte of code we do a request to Api of FacebookMessenger for get the information of user
+let id = req.body.originalDetectIntentRequest.payload.data.sender.id;
+console.log(req.body);
+//function probar(agent){
+request("https://graph.facebook.com/" + id + "?fields=first_name,last_name&access_token=EAALirSQUH18BAPHJAr6aaZAxIGXy1LMjxsMNc8DQtJHh6MDagCeHPVp5eVkD2xCZAm3IDI8yZCH43cTLEIxzP5jKbJ6LpBuPFfRJ31r72pelJUzeAZBZBXPJlOIeznmpbqovMtE9fJk9beWTf3kdQEYeB94lolfZC2AcZAz3yXpeGSv5gKbON2F", function (error, response, body) {
+  console.error('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //console.add('Hola', body.first_name); // Print the HTML for the Google homepage.
+/*    res.json({
+      "fulfillmentMessages": body.first_name
+    });*/
   });
-  
-  con.connect(function(err) {
-    if (err) {
-      console.log("Error");
-    }
-    //res.send(JSON.stringify({'Status':'OK DB'}));
-    //String query
-    var sql = "INSERT INTO base (msg) VALUES ('" + req.body.queryResult.queryText +"')";
-    con.query(sql, function (err, result) {
-      if (err) {
-        console.log("Error");
-      }
-    });
-    //res.send(JSON.stringify({'Insert':'OK'}));
-  });
-  
+ 
+functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+ 
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
+  }
+ 
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
+
+  function probarr(agent) {
+    agent.add(`Hola Daniel`);
+  }
+
+  // Run the proper function handler based on the matched Dialogflow intent name
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  intentMap.set('probar', probarr);
+  agent.handleRequest(intentMap);
 });
+
 
 //Listen port
 restService.listen(process.env.PORT || 8000, function() {
