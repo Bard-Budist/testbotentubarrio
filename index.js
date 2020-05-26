a8d460b133665f529f45e41d5b777dab8e3e5c40
danielcinome <danielchinome987@gmail.com>
2020-05-26 00:26:24 +0000

    add dialogflow

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const restService = express();
 var mysql = require('mysql');
 const request = require('request');
-const {WebhookCliente} = require('dialogflow-fulfillmet');
+const {WebhookCliente} = require('dialogflow-fulfillment');
 
 restService.use(bodyParser.json());
 
