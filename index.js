bb2b6c4865e46cd458db8ba06d26e725679664f5
Bard-Budist <op7puro@live.com>
2020-05-25 23:16:18 -0500

    Test new logic

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
 
+const API_TOKEN = "EAALirSQUH18BAPHJAr6aaZAxIGXy1LMjxsMNc8DQtJHh6MDagCeHPVp5eVkD2xCZAm3IDI8yZCH43cTLEIxzP5jKbJ6LpBuPFfRJ31r72pelJUzeAZBZBXPJlOIeznmpbqovMtE9fJk9beWTf3kdQEYeB94lolfZC2AcZAz3yXpeGSv5gKbON2F"
 const express = require("express");
 const bodyParser = require("body-parser");
 const restService = express();
 var mysql = require('mysql');
 const request = require('request');
+
 ///const {WebhookCliente} = require('dialogflow-fulfillment');
 
 restService.use(bodyParser.json());
@@ -12,19 +14,23 @@ restService.use(bodyParser.urlencoded({ extended: false }));
 
 //Endpoint home
 restService.post("/", function(req, res) {
+  let name;
   //Connection DB
 
   //const agent = new WebhookCliente({request, response});
   //console.log('Request header'+ JSON.stringify(request.header));
   //console.log('Request header', JSON.stringify(request.header));
-  
+  if (req.body.originalDetectIntentRequest.payload.data.sender.id != undefined) {
+    name = getName(req.body.originalDetectIntentRequest.payload.data.sender.id);
+  }
+
+
   res.json({
-    "fulfillmentMessages":
-    [
+    "fulfillmentMessages":[
     {
       "text":{
         "text":[
-           "Giovanni"
+           name
         ]
      },
       "platform":"FACEBOOK",
@@ -40,67 +46,30 @@ restService.post("/", function(req, res) {
     }
     ]
   });
-  //let id = req.body.originalDetectIntentRequest.payload.data.sender.id;
-  console.log(req.body);
-  //function probar(agent){
-    /*
-    request("https://graph.facebook.com/" + id + "?fields=first_name,last_name&access_token=EAALirSQUH18BAPHJAr6aaZAxIGXy1LMjxsMNc8DQtJHh6MDagCeHPVp5eVkD2xCZAm3IDI8yZCH43cTLEIxzP5jKbJ6LpBuPFfRJ31r72pelJUzeAZBZBXPJlOIeznmpbqovMtE9fJk9beWTf3kdQEYeB94lolfZC2AcZAz3yXpeGSv5gKbON2F", function (error, response, body) {
-      console.error('error:', error); // Print the error if one occurred
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-      //console.add('Hola', body.first_name); // Print the HTML for the Google homepage.
-
-      //res.json({
-      //  "fulfillmentMessages":[
-      //  {
-      //    "text":{
-      //      "text":[
-      //         body.first_name
-      //      ]
-      //   },
-      //    "platform":"FACEBOOK",
-      //    "lang":"es"
-      // },
-      //  {
-      //    "text":{
-      //      "text":[
-      //         ""
-      //      ]
-      //    },
-      //    "lang":"es"
-      //  }
-      //  ]
-      //});
-      
-    });
-    */
-    //agent.add('Hola', body.first_name);
-  //}
-
-  var con = mysql.createConnection({
-    host: "181.63.179.1",
-    user: "pepito",
-    password: "",
-    database: "messages"
-  });
   
-  con.connect(function(err) {
-    if (err) {
-      console.log("Error");
-    }
-    //res.send(JSON.stringify({'Status':'OK DB'}));
-    //String query
-    var sql = "INSERT INTO base (msg) VALUES ('" + req.body.queryResult.queryText +"')";
-    con.query(sql, function (err, result) {
-      if (err) {
-        console.log("Error");
-      }
-    });
-    //res.send(JSON.stringify({'Insert':'OK'}));
-  });
+  console.log(req.body);
   
 });
 
-//Listen port
+
+/*
+ Funtion to return name of Facebook PSID (Page Scope ID)
+*/
+function getName(PSID) {
+  if (PSID == undefined) {
+    console.log("*********Use DialogFlow Test*********");
+    return "**NameTest**";
+  }else {
+    request("https://graph.facebook.com/" + PSID + "?fields=first_name,last_name&access_token=" + API_TOKEN, function (error, response, body) {
+      if (error) {
+        console.log("STATUS CODE -> " + response.statusCode + " ERROR ->" + error);
+        return undefined;
+      }
+      return body.first_name;
+    });
+  }
+}
+
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
 });
