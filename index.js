99ab4f446b2b224aaf78f2e88ae51671edc9ba55
danielcinome <danielchinome987@gmail.com>
2020-05-26 20:18:09 +0000

    Add button in Card}

--- index.js
+++ index.js
@@ -24,13 +24,15 @@ function newSesion(agent) {
 
   return requesthttp.get("https://graph.facebook.com/" + id + "?fields=first_name,last_name&access_token=" + URLTOKEN).then(jsonBody => {
     const body = JSON.parse(jsonBody);
-    // Add response with a card and name of user
-    agent.add(new Card({
+    // Add response with a card and name of user}
+    let card = new Card({
       imageUrl: 'https://lh3.googleusercontent.com/proxy/GcA6CqAzJ94Q8GMS9RgKYkys-xXNX93K_JC0b8VuXj7oMcDcztpAX1hOZlZNfyEDQYyi12jwPBRqx1jkSuPtrl9XulREZF13ItQa2tkSWbxwfQBmQjVRqdkVNBz59ydfGWlCI8c_r4yCsgkzr4FyOagndcB1CQAhHglk6Y7nWgm_mtZjexI',
       title: body.first_name + ` Bienvenido`,
       text: 'Soy Elin el bot de EnTuBarrio',
-      button: {'text': 'Comenzar Orden'}
-    }));
+    });
+    card.setButton({ text: 'Comenzar Orden'});
+    card.setButton({ text: 'Soporte'});
+    agent.add(card);
     return Promise.resolve( agent );
 });
 }
