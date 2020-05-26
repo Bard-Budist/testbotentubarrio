a2b55f007a710fbad9294998eb4a1ae8fceb8805
Bard-Budist <op7puro@live.com>
2020-05-25 21:11:32 -0500

    Test with fulfillmentMessages

--- index.js
+++ index.js
@@ -19,16 +19,16 @@ restService.post("/", function(req, res) {
   //console.log('Request header', JSON.stringify(request.header));
   
   let id = req.body.originalDetectIntentRequest.payload.data.sender.id;
-  console.log(req);
+  console.log(req.body);
   //function probar(agent){
     request("https://graph.facebook.com/" + id + "?fields=first_name,last_name&access_token=EAALirSQUH18BAPHJAr6aaZAxIGXy1LMjxsMNc8DQtJHh6MDagCeHPVp5eVkD2xCZAm3IDI8yZCH43cTLEIxzP5jKbJ6LpBuPFfRJ31r72pelJUzeAZBZBXPJlOIeznmpbqovMtE9fJk9beWTf3kdQEYeB94lolfZC2AcZAz3yXpeGSv5gKbON2F", function (error, response, body) {
       console.error('error:', error); // Print the error if one occurred
       console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
       //console.add('Hola', body.first_name); // Print the HTML for the Google homepage.
       res.json({
-        "fulfillmentText": body.first_name
+        "fulfillmentMessages": body.first_name
       });
-      console.log(response);
+      
     });
     //agent.add('Hola', body.first_name);
   //}
