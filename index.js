7ab01ded8d61d6d38b9333683a1ee0145965833d
danielcinome <danielchinome987@gmail.com>
2020-05-30 17:24:15 +0000

    test cityBarrio

--- index.js
+++ index.js
@@ -210,8 +210,8 @@ restService.post("/", function(request, response) {
    */
   function save_cityBarrio(agent) {
     let cityBarrio = request.body.queryText;
-    console.log('esto es request ' + JSON.parse(request));
-    console.log('esto es request ' + request.toString);
+    console.log('esto es request ' + request.body.originalDetectIntentRequest.payload.data.sender.id);
+    console.log('esto es request ' + request.body.originalDetectIntentRequest.payload);
     console.log('esto es cityBarrio ' + cityBarrio);
     agent.add('esto es prubea');
     return Promise.resolve( agent );
