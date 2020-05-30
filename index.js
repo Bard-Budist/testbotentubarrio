1302c29fcd29c2a16ed094848b6a035827dab129
danielcinome <danielchinome987@gmail.com>
2020-05-30 20:38:17 +0000

    fix save_cityBarrio

--- index.js
+++ index.js
@@ -238,7 +238,7 @@ restService.post("/", function(request, response) {
     let id = request.body.originalDetectIntentRequest.payload.data.sender.id;
     database.updateWhereID(
         id,
-        'cliente'
+        'cliente',
         ['address'],
         [cityBarrio]
         );
