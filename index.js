f1c4b28a84fbe4b2525d6545ae6fa4bce79651b2
Bard-Budist <op7puro@live.com>
2020-06-06 21:21:08 -0500

    Add body request

--- index.js
+++ index.js
@@ -320,7 +320,7 @@ agent.handleRequest(intentMap);
 
 
 restService.post("/orderResponse", function(request, response){
-  console.log(request.query);
+  console.log(request.body);
   
   console.log(request.params)
   console.log(request.headers);
