8d37be03cb3a645e50f8eabe139609f1164bf033
Bard-Budist <op7puro@live.com>
2020-06-10 15:41:53 -0500

    Add log for test

--- index.js
+++ index.js
@@ -448,6 +448,7 @@ restService.post("/orderResponse", async function(request, response){
       }`
     }
   }).then(function(order) {
+    console.log(order.data.data);
     socket.emit("order", order.data.data);
   })
   
