2eaddcc7a4ead0a8388f69c5549604f7608ad502
danielcinome <danielchinome987@gmail.com>
2020-06-15 22:32:30 +0000

    fix obtain psid

--- index.js
+++ index.js
@@ -445,24 +445,26 @@ restService.post("/orderResponse", async function(request, response){
 
 socket.on('OrderForBot', function(idOrder) {
   console.log('Tendero Acepto la Orden No: ', idOrder);
+  console.log(idOrder.order);
+  console.log(idOrder['order']);
   const psid = "";
-  graphQl({
-    url: url,
-    method: 'post',
-    data: {
-      query: `{
-          order(id: ${idOrder.order}){
-            client {
-              id,
-              name,
-              address,
-            },
-            products
-          }`
-        }
-      }).then(function(result) {
-      console.log(result);
-    });
+  // graphQl({
+  //   url: url,
+  //   method: 'post',
+  //   data: {
+  //     query: `{
+  //         order(id: ${idOrder.order}){
+  //           client {
+  //             id,
+  //             name,
+  //             address,
+  //           },
+  //           products
+  //         }`
+  //       }
+  //     }).then(function(result) {
+  //     console.log(result);
+  //   });
   // database.selectAllByID(idOrder, 'order', ["client {id, name, address,}, products})"]
   // ).then(function (result) {
   //   console.log(result.data);
